eee6f3df951cb2bf5808d2b4c477a64075984a12
jingcheng <jingcheng070@gmail.com>
2023-09-08 18:05:00 +0800

    Add updateStatus

--- controller/auth.js
+++ controller/auth.js
@@ -74,3 +74,45 @@ exports.login = (req, res, next) => {
       next(err);
     });
 };
+
+exports.getUserStatus = (req, res, next) => {
+  console.log(req.userId);
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error("No User found");
+        error.statusCode = 401;
+        throw error;
+      }
+      res.status(200).json({ status: user.status });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.updateUserStatus = (req, res, next) => {
+  const newStatus = req.body.status;
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error("No User Found");
+        error.statusCode = 401;
+        throw error;
+      }
+      user.status = newStatus;
+      return user.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "User updated!" });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const { body } = require("express-validator");
 const User = require("../models/user");
 const authController = require("../controller/auth");
 
+const isAuth = require("../middleware/is-auth");
+
 const router = express.Router();
 
 router.put(
@@ -28,4 +30,12 @@ router.put(
 
 router.post("/login", authController.login);
 
+router.get("/status", isAuth, authController.getUserStatus);
+
+router.patch(
+  "/status",
+  isAuth,
+  [body("status").trim().not().isEmpty()],
+  authController.updateUserStatus
+);
 module.exports = router;
