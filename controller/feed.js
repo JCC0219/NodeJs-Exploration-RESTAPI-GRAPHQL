17d8ac7c4db917fa1b822947595b0617c018d270
jingcheng <jingcheng070@gmail.com>
2023-09-08 12:39:07 +0800

    add pagination

--- controller/feed.js
+++ controller/feed.js
@@ -5,11 +5,25 @@ const path = require("path");
 const { clear } = require("console");
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
       res
         .status(200)
-        .json({ message: "Fetched posts successfully.", posts: posts });
+        .json({
+          message: "Fetched posts successfully.",
+          posts: posts,
+          totalItems: totalItems,
+        });
     })
     .catch((err) => {
       if (!err.statusCode) {
@@ -138,7 +152,7 @@ exports.deletePost = (req, res, next) => {
     })
     .then((result) => {
       console.log(result);
-      return res.status(200).json({ message: "Deleted post."});
+      return res.status(200).json({ message: "Deleted post." });
     })
     .catch((err) => {
       if (!err.statusCode) {
